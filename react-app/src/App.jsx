97548add98354d6df96c167f510603cd4c06ecda
RohanK-coder <kyrohan7730@gmail.com>
2024-08-15 22:39:10 +0530

    From here on the work will be saved in react-app folder : Check the pages in the react app for more info

---
+++ react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomComponents from './pages/CustomComponents'
+
+export default function App() {
+  return (
+    <>
+    <div>Rohan Kommathoti's React Application</div>
+    <CustomComponents/>
+    </>
+  )
+}
